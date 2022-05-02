fc9975acd8924a486f09cf0e7dabee69bdf78cee
Fabian Lien <fabian.lien@gmail.com>
2022-05-02 10:01:30 +0000

    replace secret key

--- django_demo/settings.py
+++ django_demo/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-@asht(dpfqa4-)&x%1h53rcd68=zk__6aftxus#7tqnxhtf@j+')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
