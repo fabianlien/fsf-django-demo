fdb70efacbd8833d6c931030447841cd79e5ddad
Fabian Lien <fabian.lien@gmail.com>
2022-05-02 08:42:20 +0000

    update allowed hosts in settings.py

--- django_demo/settings.py
+++ django_demo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@asht(dpfqa4-)&x%1h53rcd68=zk__6aftxus#7tqnxhtf@j+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fsf-django-demo.herokuapp.com']
 
 
 # Application definition
